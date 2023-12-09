a2bbf1b79de395de50ee6dddb2e8aa01b8f0af7f
Rita0156 <ritachaudhari006@gmail.com>
2023-12-09 16:09:37 +0530

    next

--- Unit-3/index.js
+++ Unit-3/index.js
@@ -1,5 +1,7 @@
 let container=document.getElementById("container")
 let buttonWrapper=document.getElementById("button-wrapper")
+let next=document.getElementById("next")
+let prev=document.getElementById("prev")
 
 function pagination(data,page,limit){
       return data.slice((page-1)*limit,(page*limit-1)+1)
@@ -14,8 +16,12 @@ async function fetchData(){
      let total=Math.ceil(t/limit)
      buttonWrapper.innerHTML=null
      for(let i=1; i<=total; i++){
+      //buttonWrapper.append(createBtn(i))
+      
       buttonWrapper.append(createBtn(i))
+      
      }
+     
      let resArr =pagination(data, 1, limit);
         appendData(resArr);
 
@@ -41,17 +47,54 @@ setTimeout(async function(){
   let res = await fetch("https://fakestoreapi.com/products");
   let data = await res.json();
   let limit = 4;
-
+  var ind=1
+ let total=(data.length/limit)
   btns.forEach((btn) => {
       btn.addEventListener("click",function(){
-          btns.forEach(btnb=> btnb.classList.remove('active'));
-          btn.classList.add("active")
+        btn.forEach(btnb=> btnb.classList.remove('active'));
+        btn.classList.add("active")
           let i = btn.innerText;
+          ind=btn.innerText;
           let resArr = pagination(data, i, limit);
           appendData(resArr);
       })
   })
+  next.addEventListener("click",()=>{
+    //buttonWrapper.innerHTML=null
+    //for(let i=1; i<=total; i++){
+      if(ind==total){
+        next.disabled=true;
+        next.style.color="grey"
+      }else if(ind<total){
+        //btns[ind].forEach(btnb=> btnb.classList.remove('active'));
+          //btns[ind].classList.add("active")
+          console.log(ind,ind)
+        ind=ind+1
+         //ind = btns[ind].innerText;
+        let resArr = pagination(data, ind, limit);
+        appendData(resArr);
+      }
+    //}
+    
+  })
+  prev.addEventListener("click",()=>{
+    if(ind==1 || ind<1){
+      prev.disabled=true;
+      prev.style.color="grey"
+    }else if(ind<total){
+      //btns[ind].forEach(btnb=> btnb.classList.remove('active'));
+        //btns[ind].classList.add("active")
+        console.log(ind,ind)
+      ind=ind-1
+       //ind = btns[ind].innerText;
+      let resArr = pagination(data, ind, limit);
+      appendData(resArr);
+    }
+  })
 },500)
+//for(let i=1; i<=total; i++){
+  
+//}
 
 function appendData(data){
   container.innerHTML=null
